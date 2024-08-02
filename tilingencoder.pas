fc6231df527b3e833805480607b56d139165c2b7
GliGli <sfx.gligli@free.fr>
2024-08-02 20:53:05 +0200

    OptimizePalettes: optimisations, integer calculations (inproved precision)

--- tilingencoder.pas
+++ tilingencoder.pas
@@ -4506,17 +4506,18 @@ end;
 procedure TTilingEncoder.OptimizePalettes;
 var
   Bests: TDoubleDynArray;
-  BestsPalIdx, BestsColIdx1, BestsColIdx2: TIntegerDynArray;
+  BestsColIdx1, BestsColIdx2: TIntegerDynArray;
 
   procedure DoPal(AIndex: PtrInt; AData: Pointer; AItem: TMultiThreadProcItem);
   var
     i, j, colIdx1, colIdx2, uc: Integer;
-    r, g, b: byte;
-    best, v: Double;
-    bestPalIdx, bestColIdx1, bestColIdx2: Integer;
+    r, g, b: Byte;
+    rr, gg, bb: UInt64;
+    best, v, MeanR, MeanG, MeanB, StdDevR, StdDevG, StdDevB: Double;
+    bestColIdx1, bestColIdx2: Integer;
 
     InnerPerm: array[0 .. Sqr(cTileWidth) - 1] of Integer;
-    PalR, PalG, PalB, InnerPalR, InnerPalG, InnerPalB: array[0 .. Sqr(cTileWidth) - 1] of Double;
+    PalR, PalG, PalB, InnerPalR, InnerPalG, InnerPalB: array[0 .. Sqr(cTileWidth) - 1] of UInt64;
   begin
     if not InRange(AIndex, 0, FPaletteCount - 1) then
       Exit;
@@ -4542,47 +4543,75 @@ var
     // try all permutations in the current palette
 
     best := 0;
-    bestPalIdx := -1;
     bestColIdx1 := -1;
     bestColIdx2 := -1;
+
+    uc := FPalettes[AIndex].UseCount;
+    for i := 0 to FPaletteSize - 1 do
+      InnerPerm[i] := i;
 
     for colIdx1 := 0 to FPaletteSize - 1 do
       for colIdx2 := colIdx1 + 1 to FPaletteSize - 1 do
       begin
-        for i := 0 to FPaletteSize - 1 do
-          InnerPerm[i] := i;
-
         Exchange(InnerPerm[colIdx1], InnerPerm[colIdx2]);
 
-        Move(PalR[0], InnerPalR[0], FPaletteSize * SizeOf(Double));
-        Move(PalG[0], InnerPalG[0], FPaletteSize * SizeOf(Double));
-        Move(PalB[0], InnerPalB[0], FPaletteSize * SizeOf(Double));
+        Move(PalR[0], InnerPalR[0], FPaletteSize * SizeOf(UInt64));
+        Move(PalG[0], InnerPalG[0], FPaletteSize * SizeOf(UInt64));
+        Move(PalB[0], InnerPalB[0], FPaletteSize * SizeOf(UInt64));
 
-        uc := FPalettes[AIndex].UseCount;
+        MeanR := 0;
+        MeanG := 0;
+        MeanB := 0;
         for i := 0 to FPaletteSize - 1 do
         begin
           FromRGB(FPalettes[AIndex].PaletteRGB[InnerPerm[i]], r, g, b);
-          InnerPalR[i] += r * uc;
-          InnerPalG[i] += g * uc;
-          InnerPalB[i] += b * uc;
+
+          rr := r * uc + InnerPalR[i];
+          gg := g * uc + InnerPalG[i];
+          bb := b * uc + InnerPalB[i];
+
+          MeanR += rr;
+          MeanG += gg;
+          MeanB += bb;
+
+          InnerPalR[i] := rr;
+          InnerPalG[i] := gg;
+          InnerPalB[i] := bb;
         end;
 
         // try to maximize accumulated palette standard deviation
-        // rationale: the less samey it is, the better the colors pair with each other across FPalettes
+        // rationale: the less samey it is, the better the colors pair with each other across palette
 
-        v := cRedMul * StdDev(InnerPalR, FPaletteSize) + cGreenMul * StdDev(InnerPalG, FPaletteSize) + cBlueMul * StdDev(InnerPalB, FPaletteSize);
+        MeanR /= FPaletteSize;
+        MeanG /= FPaletteSize;
+        MeanB /= FPaletteSize;
+        StdDevR := 0;
+        StdDevG := 0;
+        StdDevB := 0;
+        for i := 0 to FPaletteSize - 1 do
+        begin
+          StdDevR += Sqr(InnerPalR[i] - MeanR);
+          StdDevG += Sqr(InnerPalG[i] - MeanG);
+          StdDevB += Sqr(InnerPalB[i] - MeanB);
+        end;
+        StdDevR := Sqrt(StdDevR / FPaletteSize);
+        StdDevG := Sqrt(StdDevG / FPaletteSize);
+        StdDevB := Sqrt(StdDevB / FPaletteSize);
+
+        v := cRedMul * StdDevR + cGreenMul * StdDevG + cBlueMul * StdDevB;
 
         if v > best then
         begin
           best := v;
-          bestPalIdx := AIndex;
           bestColIdx1 := colIdx1;
           bestColIdx2 := colIdx2;
         end;
+
+        // "repair" what was changed
+        Exchange(InnerPerm[colIdx1], InnerPerm[colIdx2]);
       end;
 
     Bests[AIndex] := best;
-    BestsPalIdx[AIndex] := bestPalIdx;
     BestsColIdx1[AIndex] := bestColIdx1;
     BestsColIdx2[AIndex] := bestColIdx2;
   end;
@@ -4593,7 +4622,6 @@ var
   bestPalIdx, bestColIdx1, bestColIdx2: Integer;
 begin
   SetLength(Bests, FPaletteCount);
-  SetLength(BestsPalIdx, FPaletteCount);
   SetLength(BestsColIdx1, FPaletteCount);
   SetLength(BestsColIdx2, FPaletteCount);
 
@@ -4614,7 +4642,7 @@ begin
       if Bests[palIdx] > best then
       begin
         best := Bests[palIdx];
-        bestPalIdx := BestsPalIdx[palIdx];
+        bestPalIdx := palIdx;
         bestColIdx1 := BestsColIdx1[palIdx];
         bestColIdx2 := BestsColIdx2[palIdx];
       end;
